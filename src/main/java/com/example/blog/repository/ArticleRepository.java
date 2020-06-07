79d665843b107c63d3ba6cdc4c0b6c2fbc5cd2fe
Lubov Agareva <agareva.lubov@gmail.com>
2020-06-07 03:09:18 +0300

    Added actions with article

--- src/main/java/com/example/blog/repository/ArticleRepository.java
+++ src/main/java/com/example/blog/repository/ArticleRepository.java
@@ -1,4 +1,14 @@
 package com.example.blog.repository;
 
-public class ArticleRepository {
+import com.example.blog.model.Article;
+import com.example.blog.model.Status;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface ArticleRepository extends JpaRepository<Article, Integer> {
+    Optional<List<Article>> findAllByStatus(Status status);
 }
